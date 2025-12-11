f39f62b8b6272aededaf2deafaa8e7d0745d864b
Aviel Oiknine <avieloikpro@gmail.com>
2025-12-11 10:10:57 +0200

    filter and searche features

---
+++ services/filterFeature.js
@@ -0,0 +1,13 @@
+import { stockMarket } from "../dataBase/stoks.js"
+
+export function filterStocksByPrice(givenPrice, above) {
+    if (isNaN(givenPrice)) {
+        console.log("You need to enter a number !")
+        return
+    }
+    const givenPriceInt = parseInt(givenPrice)
+    let searchResult = stockMarket.stocks.filter((stock) => above ? stock.currentPrice >= givenPriceInt : stock.currentPrice < givenPriceInt)
+    if (searchResult.length === 0)
+        console.log("No stock matches the provided data")
+    return searchResult
+}
---
+++ services/searchFeature.js
@@ -0,0 +1,11 @@
+import { stockMarket } from "../dataBase/stoks.js"
+
+export function searchStock(identifier) {
+    let searchResult = []
+    for (let stock of stockMarket.stocks)
+        if (identifier === stock.id || identifier === stock.name)
+            searchResult.push(stock)
+    if (searchResult.length === 0)
+        console.log("No stock matches the provided identifier")
+    return searchResult
+}
