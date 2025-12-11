a71ad7bad82218cf8701ec37a4eea56a2ea15e02
Aviel Oiknine <avieloikpro@gmail.com>
2025-12-11 12:49:02 +0200

    bonus not finish

---
+++ services/marketTrendAnalyzer.js
@@ -0,0 +1,26 @@
+import { stockMarket } from "../dataBase/stoks.js"
+
+export default function analyzeMarketTrends() {
+    let increasStocks = topStocks().sort((a, b) => b.growth - a.growth).slice(3)
+    let decreasStocks = topStocks().sort((a, b) => a.growth - b.growth).slice(3)
+    let result = {
+        topIncreasingStocks: increasStocks,
+        topDecreasingStocks: decreasStocks,
+        mostVolatileStock: null,
+        categoryStability: null
+    }
+
+}
+
+
+function topStocks() {
+    let topStock = []
+    for (let stock of stockMarket.stocks) {
+        if (stock.previousPrices.length === 0)
+            return
+        let growth = stock.currentPrice - stock.previousPrices[0]
+        topStock.push({ stock: stock, growth: growth })
+    }
+    return topStock
+}
+
